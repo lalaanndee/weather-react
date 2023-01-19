af21a42951e8c0cc2c4cb4b8252c96395d24ea8b
lalaanndee <lanissadavis@gmail.com>
2023-01-18 22:16:19 -0500

    Added styling

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,7 +10,7 @@ export default function WeatherForecastDay(props) {
     return `${temperature}°`;
   }
   function day() {
-    let date = new Date(props.data.day * 1000);
+    let date = new Date(props.data.time * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return days[day];
@@ -18,7 +18,9 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <img src={props.data.icon_url} alt={props.data.description} />
+      <div>
+        <img src={props.data.icon_url} alt={props.data.description} size={10} />
+      </div>
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-maximum">
           {maxTemperature()}
